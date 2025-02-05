2522512db1e7c47185b7f7060e60188c4f3a8b06
RanceCL <roderickrwhite2022@gmail.com>
2025-02-05 17:14:41 -0500

    Added language model to config.

--- config.py
+++ config.py
@@ -5,4 +5,5 @@ from datetime import timedelta
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://'+os.environ["POSTRGRES_USER"]+':'+os.environ["POSTRGRES_PASSWORD"]+'@'+os.environ["POSTRGRES_HOST"]+'/'+os.environ["POSTRGRES_DATABASE"]
+    LANGUAGE_MODEL = "gpt-4o-mini"
     #PERMANENT_SESSION_LIFETIME = timedelta(minutes=1)
