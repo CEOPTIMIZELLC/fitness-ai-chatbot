3784f618a55ecf855866f1a45bdb010df6f35383
RanceCL <roderickrwhite2022@gmail.com>
2025-02-17 13:28:32 -0500

    Added test data for exercise equipment.

---
+++ app/existing_data/exercise_equipment.py
@@ -0,0 +1,6 @@
+from app.models import Exercise_Equipment
+exercise_equipment = [
+    Exercise_Equipment(exercise_id=1,equipment_id=1),
+    Exercise_Equipment(exercise_id=2,equipment_id=1),
+    Exercise_Equipment(exercise_id=2,equipment_id=2)
+]
