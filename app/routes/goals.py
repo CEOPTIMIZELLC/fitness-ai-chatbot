e009690074534aac0b01dc2d21318d18ada65258
RanceCL <roderickrwhite2022@gmail.com>
2025-02-24 13:30:48 -0500

    Added route to change current user's goal.

--- app/routes/goals.py
+++ app/routes/goals.py
@@ -1,4 +1,5 @@
 from flask import request, jsonify, Blueprint
+from flask_login import current_user, login_required
 
 from app.models import Goal_Library
 
@@ -25,8 +26,29 @@ def read_goal(goal_id):
         return jsonify({"status": "error", "message": "Goal " + goal_id + " not found."}), 404
     return jsonify(goal.to_dict()), 200
 
+# Change the current user's goal.
+@bp.route('/', methods=['POST', 'PATCH'])
+@login_required
+def change_goal():
+    # Input is a json.
+    data = request.get_json()
+    if not data:
+        return jsonify({"status": "error", "message": "Invalid request"}), 400
+    
+    # Ensure that a goal has been provided.
+    if ('goal' not in data):
+        return jsonify({"status": "error", "message": "Please fill out the form!"}), 400
+    
+    result_temp = goal_app.invoke({"new_goal": data.get("goal", ""), "attempts": 0})
+    return jsonify({
+        "new_goal": result_temp["new_goal"],
+        "goal_classification": result_temp["goal_class"],
+        "goal_id": result_temp["goal_id"]
+    }), 200
+
+
 # Testing for goal classification.
-@bp.route('/goal_classification', methods=['GET'])
+@bp.route('/goal_classification', methods=['GET', 'POST'])
 def goal_classification():
     results = {}
     
@@ -39,7 +61,7 @@ def goal_classification():
     print(f"Result: '{result_temp["goal_class"]}' with id of '{str(result_temp["goal_id"])}'")
     print("")
 
-    
+
     user_goal = "I would like to do a push up."
     result_temp = goal_app.invoke({"new_goal": user_goal, "attempts": 0})
     results["result_2"] = {
