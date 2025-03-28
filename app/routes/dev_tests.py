6939218425efd7c86b8311c9e12aa29fb2aa78fe
RanceCL <roderickrwhite2022@gmail.com>
2025-03-28 14:55:18 -0400

    Added routes to run the pipeline and read all values that would be initialized from it.

--- app/routes/dev_tests.py
+++ app/routes/dev_tests.py
@@ -1,5 +1,8 @@
 from random import randrange
 from flask import request, jsonify, redirect, url_for, current_app, Blueprint
+
+from flask_login import current_user, login_required
+
 from flask_cors import CORS
 from sqlalchemy import text
 
@@ -21,6 +24,44 @@ from app.agents.cp_ortools import Main as cp_ortools_main
 #from app.helper_functions.cp_pulp_equipment_test import Main as cp_pulp_equipment_test_main
 
 # ----------------------------------------- Dev Tests -----------------------------------------
+
+# Quick check of the entire pipeline
+@bp.route('/pipeline', methods=['GET'])
+@login_required
+def check_pipeline():
+    from app.routes.user_macrocycles import get_user_current_macrocycle
+    from app.routes.user_mesocycles import get_user_current_mesocycles
+    from app.routes.user_microcycles import get_user_current_microcycles
+    from app.routes.user_workout_days import get_user_current_workout_days
+    from app.routes.user_exercises import get_user_current_exercises
+
+    result = {}
+    result["user_macrocycles"] = get_user_current_macrocycle()[0].get_json()["goals"]
+    result["user_mesocycles"] = get_user_current_mesocycles()[0].get_json()["phases"]
+    result["user_microcycles"] = get_user_current_microcycles()[0].get_json()["microcycles"]
+    result["user_workout_days"] = get_user_current_workout_days()[0].get_json()["phase_components"]
+    result["user_exercises"] = get_user_current_exercises()[0].get_json()["exercises"]
+    return result
+
+
+# Quick test of the entire pipeline
+@bp.route('/pipeline', methods=['POST'])
+@login_required
+def run_pipeline():
+    from app.routes.user_macrocycles import change_goal
+    from app.routes.user_mesocycles import mesocycle_phases
+    from app.routes.user_microcycles import microcycle_initializer
+    from app.routes.user_workout_days import workout_day_initializer
+    from app.routes.user_exercises import exercise_initializer
+
+    result = {}
+    result["user_macrocycles"] = change_goal()[0].get_json()
+    result["user_mesocycles"] = mesocycle_phases()[0].get_json()["mesocycles"]["output"]
+    result["user_microcycles"] = microcycle_initializer()[0].get_json()["microcycles"]
+    result["user_workout_days"] = workout_day_initializer()[0].get_json()["workdays"]["output"]
+    result["user_exercises"] = exercise_initializer()[0].get_json()["exercises"]["output"]
+
+    return result
 
 # Testing for the SQL to add and check training equipment.
 @bp.route('/test_equipment_sql', methods=['GET'])
