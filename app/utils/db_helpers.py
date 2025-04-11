799869ca3642c4b39f93a318e1a6fdaec2f7ee86
RanceCL <roderickrwhite2022@gmail.com>
2025-04-11 15:42:13 -0400

    https://app.usemotion.com/web/pm/workspaces/mItBy-ORI682KWmH3G1Bu?task=_W-zqy6ccyKkLPdzDzTFv
    Added method to attempt to retrieve an entry and create said entry if it doesn't exist.

--- app/utils/db_helpers.py
+++ app/utils/db_helpers.py
@@ -1,5 +1,6 @@
 from typing import List, Dict, Any, Optional
 from app import db
+from sqlalchemy.exc import IntegrityError, NoResultFound
 
 def get_all_items(model_class) -> List[Dict[str, Any]]:
     """Generic function to get all items from a model class"""
@@ -12,3 +13,20 @@ def get_item_by_id(model_class, item_id) -> Optional[Dict[str, Any]]:
     if not item:
         return None
     return item.to_dict()
+
+def get_or_create(session, model, defaults=None, **kwargs):
+	"""
+	Get or create a model instance while preserving integrity.
+	"""
+	try:
+		return session.query(model).filter_by(**kwargs).one(), False
+	except NoResultFound:
+		if defaults is not None:
+			kwargs.update(defaults)
+		try:
+			with session.begin_nested():
+				instance = model(**kwargs)
+				session.add(instance)
+				return instance, True
+		except IntegrityError:
+			return session.query(model).filter_by(**kwargs).one(), False
