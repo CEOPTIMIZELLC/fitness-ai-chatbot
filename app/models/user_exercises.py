9a173631b351a90658d4069c1ea42d996d093315
RanceCL <roderickrwhite2022@gmail.com>
2025-04-11 17:31:03 -0400

    https://app.usemotion.com/web/pm/workspaces/mItBy-ORI682KWmH3G1Bu?task=_W-zqy6ccyKkLPdzDzTFv
    Corrected model so that the user id and exercise id are the primary keys.

--- app/models/user_exercises.py
+++ app/models/user_exercises.py
@@ -2,13 +2,13 @@ from app import db
 from app.models.base import BaseModel
 from app.models.mixins import TableNameMixin
 
-class User_Exercises(BaseModel, TableNameMixin):
+class User_Exercises(db.Model, TableNameMixin):
     """Exercise available to a user during a training period."""
     __table_args__ = {'comment': "The exercises that a user has performed."}
 
     # Fields
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id", ondelete='CASCADE'), nullable=False)
-    exercise_id = db.Column(db.Integer, db.ForeignKey("exercise_library.id", ondelete='CASCADE'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id", ondelete='CASCADE'), primary_key=True)
+    exercise_id = db.Column(db.Integer, db.ForeignKey("exercise_library.id", ondelete='CASCADE'), primary_key=True)
     one_rep_max = db.Column(db.Integer, nullable=False, default=5, comment='The maximum weight that the user can lift for one repetition of the exercise.')
 
     # Relationships
@@ -17,7 +17,6 @@ class User_Exercises(BaseModel, TableNameMixin):
 
     def to_dict(self):
         return {
-            "id": self.id, 
             "user_id": self.user_id, 
             "exercise_id": self.exercise_id, 
             "exercise_name": self.exercises.name,
