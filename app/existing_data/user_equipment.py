b164f6a15481c753aa4072f1b9d83509999b678b
RanceCL <roderickrwhite2022@gmail.com>
2025-04-23 15:49:53 -0400

    https://app.usemotion.com/web/pm/workspaces/mItBy-ORI682KWmH3G1Bu?task=JK6HCh3EkNtPysTEdH-47
    Changed test for user equipment for easier testing.

--- app/existing_data/user_equipment.py
+++ app/existing_data/user_equipment.py
@@ -2,5 +2,7 @@ from app.models import User_Equipment
 user_equipment = [
     User_Equipment(user_id=1, equipment_id=1, measurement=10),
     User_Equipment(user_id=1, equipment_id=3, measurement=45),
+    User_Equipment(user_id=1, equipment_id=4, measurement=45),
+    User_Equipment(user_id=1, equipment_id=17, measurement=40),
     User_Equipment(user_id=1, equipment_id=17, measurement=45)
 ]
