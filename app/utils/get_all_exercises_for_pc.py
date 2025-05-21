7a1ceb81d239b5922ef1741db5988ae710492513
RanceCL <roderickrwhite2022@gmail.com>
2025-05-21 14:41:46 -0400

    https://app.usemotion.com/web/pm/workspaces/mItBy-ORI682KWmH3G1Bu?task=8rwmJu0CrZgrvk6ixmLQP
    Changed how the exercises for phase component method would determine if it should print the verbose log.

--- app/utils/get_all_exercises_for_pc.py
+++ app/utils/get_all_exercises_for_pc.py
@@ -1,45 +1,37 @@
+from config import verbose
+
 def get_exercises_for_pc_conditions(exercises, phase_component, conditions=[]):
     return [i for i, exercise in enumerate(exercises, start=1) 
             if all(f(exercise, phase_component) for f in conditions)]
 
-def get_exercises_for_pc(exercises, phase_component, verbose=False):
+def get_exercises_for_pc(exercises, phase_component):
     conditions = [lambda exercise, phase_component: phase_component["pc_ids"] in exercise["pc_ids"],
                   # lambda exercise, phase_component: phase_component["component_id"] in exercise["component_ids"],
                   # lambda exercise, phase_component: phase_component["subcomponent_id"] in exercise["subcomponent_ids"],
                   lambda exercise, phase_component: (1 in exercise["bodypart_ids"]) or (phase_component["bodypart_id"] in exercise["bodypart_ids"])]
 
     exercises_for_pc = get_exercises_for_pc_conditions(exercises, phase_component, conditions)
-    print_check = False
+
+    message = None
 
     if (exercises_for_pc == []) and (phase_component["bodypart_id"] == 1):
-        if verbose: 
-            print(f"'{phase_component['phase_name']} {phase_component['component_name']} {phase_component['subcomponent_name']}' has no exercises for bodypart '{phase_component['bodypart_name']}', include all exercises for this component phase if it's total body.")
-            print_check = True
+        message = f"'{phase_component['phase_name']} {phase_component['component_name']} {phase_component['subcomponent_name']}' has no exercises for bodypart '{phase_component['bodypart_name']}'. If total body, include all exercises for this component phase."
         exercises_for_pc = get_exercises_for_pc_conditions(exercises, phase_component, conditions[0:1])
 
     if exercises_for_pc == []:
-        if verbose: 
-            print(f"'{phase_component['phase_name']} {phase_component['component_name']} {phase_component['subcomponent_name']}' still has no exercises for bodypart '{phase_component['bodypart_name']}', include all exercises for this component phase.")
-            print_check = True
-        exercises_for_pc = get_exercises_for_pc_conditions(exercises, phase_component, conditions[0:1])
+        message = f"'{phase_component['phase_name']} {phase_component['component_name']} {phase_component['subcomponent_name']}' still has no exercises for bodypart '{phase_component['bodypart_name']}'."
 
-    if exercises_for_pc == []:
-        if verbose: 
-            print(f"'{phase_component['phase_name']} {phase_component['component_name']} {phase_component['subcomponent_name']}' still has no exercises for bodypart '{phase_component['bodypart_name']}', include all exercises.")
-            print_check = True
-        exercises_for_pc = get_exercises_for_pc_conditions(exercises, phase_component)
-    
-    if print_check and verbose:
-        print("")
+    if message and verbose:
+        print(message)
     return exercises_for_pc
 
 # A method for retrieving the possible exercises for all phase components.
 # This method removes exercises specific for a phase commponent from those that are allowed for a phase component without any true exercises.
-def get_exercises_for_all_pcs(exercises, phase_components, verbose=False):
+def get_exercises_for_all_pcs(exercises, phase_components):
     number_of_possible_exercises = len(exercises)
 
     exercises_for_pcs = [
-        get_exercises_for_pc(exercises, phase_component, verbose)
+        get_exercises_for_pc(exercises, phase_component)
         for phase_component in phase_components
     ]
 
