4139e9263056c9384472b8d24de0367658d87147
RanceCL <roderickrwhite2022@gmail.com>
2025-07-04 15:32:03 -0400

    https://app.usemotion.com/web/pm/workspaces/mItBy-ORI682KWmH3G1Bu?task=V7kptolyh5bCb2qtt-nBD
    Parallelized macrocycle and availability initializers.

--- app/main_agent/graph.py
+++ app/main_agent/graph.py
@@ -21,16 +21,27 @@ class AgentState(TypedDict):
 
     availability_impacted: bool
     availability_message: str
+    availability_formatted: str
+
     macrocycle_impacted: bool
     macrocycle_message: str
+    macrocycle_formatted: str
+
     mesocycle_impacted: bool
     mesocycle_message: str
+    mesocycle_formatted: str
+
     microcycle_impacted: bool
     microcycle_message: str
+    microcycle_formatted: str
+
     phase_component_impacted: bool
     phase_component_message: str
+    phase_component_formatted: str
+
     workout_schedule_impacted: bool
     workout_schedule_message: str
+    workout_schedule_formatted: str
 
 from .impact_goal_models import RoutineImpactGoals
 
@@ -82,160 +93,117 @@ def user_input_information_extraction(state: AgentState):
 
     return state
 
-def check_availability_node(state: AgentState):
-    print(f"Checking User Availability")
-    state["check"] = state["availability_impacted"]
-    return state
-
 def availability_node(state: AgentState):
     print(f"Changing User Availability")
-    availability_message = state["availability_message"]
+    if state["availability_impacted"]:
+        availability_message = state["availability_message"]
+        new_availability = WeekdayAvailabilitySchedulerActions.scheduler(availability_message)
+    else:
+        availability_message = None
     print(f"{availability_message}")
-    new_availability = WeekdayAvailabilitySchedulerActions.scheduler(availability_message)
-    return state
-
-def check_macrocycle_node(state: AgentState):
-    print(f"Checking User Macrocycle")
-    state["check"] = state["macrocycle_impacted"]
-    return state
+    return {"availability_formatted": availability_message}
 
 def macrocycle_node(state: AgentState):
     print(f"Changing User Macrocycle")
-    macrocycle_message = state["macrocycle_message"]
+    if state["macrocycle_impacted"]:
+        macrocycle_message = state["macrocycle_message"]
+        macrocycles = MacrocycleActions.scheduler(macrocycle_message)
+    else:
+        macrocycle_message = None
     print(f"{macrocycle_message}")
-    macrocycles = MacrocycleActions.scheduler(macrocycle_message)
-    return state
-
-def check_mesocycle_node(state: AgentState):
-    print(f"Checking User Mesocycle")
-    state["check"] = state["mesocycle_impacted"]
-    return state
+    return {"macrocycle_formatted": macrocycle_message}
 
 def mesocycle_node(state: AgentState):
     print(f"Changing User Mesocycle")
-    mesocycle_message = state["mesocycle_message"]
+    if state["mesocycle_impacted"]:
+        mesocycle_message = state["mesocycle_message"]
+        result = MesocycleActions.scheduler()
+        result["formatted_schedule"] = MesocycleActions.get_formatted_list()
+    else:
+        mesocycle_message = None
     print(f"{mesocycle_message}")
-    result = MesocycleActions.scheduler()
-    result["formatted_schedule"] = MesocycleActions.get_formatted_list()
-    return state
-
-def check_microcycle_node(state: AgentState):
-    print(f"Checking User Microcycle")
-    state["check"] = state["microcycle_impacted"]
-    return state
+    return {"mesocycle_formatted": mesocycle_message}
 
 def microcycle_node(state: AgentState):
     print(f"Changing User Microcycle")
-    microcycle_message = state["microcycle_message"]
+    if state["microcycle_impacted"]:
+        microcycle_message = state["microcycle_message"]
+        result = MicrocycleActions.scheduler()
+    else:
+        microcycle_message = None
     print(f"{microcycle_message}")
-    result = MicrocycleActions.scheduler()
-    return state
-
-def check_phase_component_node(state: AgentState):
-    print(f"Checking User Phase Component")
-    state["check"] = state["phase_component_impacted"]
-    return state
+    return {"microcycle_formatted": microcycle_message}
 
 def phase_component_node(state: AgentState):
     print(f"Changing User Phase Component")
-    phase_component_message = state["phase_component_message"]
+    if state["phase_component_impacted"]:
+        phase_component_message = state["phase_component_message"]
+        result = MicrocycleSchedulerActions.scheduler()
+        result["formatted_schedule"] = MicrocycleSchedulerActions.get_formatted_list()
+    else:
+        phase_component_message = None
     print(f"{phase_component_message}")
-    result = MicrocycleSchedulerActions.scheduler()
-    result["formatted_schedule"] = MicrocycleSchedulerActions.get_formatted_list()
-    return state
-
-def check_workout_schedule_node(state: AgentState):
-    print(f"Checking User Workout Schedule")
-    state["check"] = state["workout_schedule_impacted"]
-    return state
+    return {"phase_component_formatted": phase_component_message}
 
 def workout_schedule_node(state: AgentState):
     print(f"Changing User Workout Schedule")
-    workout_schedule_message = state["workout_schedule_message"]
+    if state["workout_schedule_impacted"]:
+        workout_schedule_message = state["workout_schedule_message"]
+        result = WorkoutActions.scheduler()
+        result["formatted_schedule"] = WorkoutActions.get_formatted_list()
+    else:
+        workout_schedule_message = None
     print(f"{workout_schedule_message}")
-    result = WorkoutActions.scheduler()
-    result["formatted_schedule"] = WorkoutActions.get_formatted_list()
-    return state
+    return {"workout_schedule_formatted": workout_schedule_message}
 
 def print_schedule_node(state: AgentState):
     print(f"Printing Schedule")
     print(f"Goals extracted.")
     if state["availability_impacted"]:
-        print(f"availability: {state["availability_message"]}")
+        print(f"availability: {state["availability_formatted"]}")
     if state["macrocycle_impacted"]:
-        print(f"macrocycle: {state["macrocycle_message"]}")
+        print(f"macrocycle: {state["macrocycle_formatted"]}")
     if state["mesocycle_impacted"]:
-        print(f"mesocycle: {state["mesocycle_message"]}")
+        print(f"mesocycle: {state["mesocycle_formatted"]}")
     if state["microcycle_impacted"]:
-        print(f"microcycle: {state["microcycle_message"]}")
+        print(f"microcycle: {state["microcycle_formatted"]}")
     if state["phase_component_impacted"]:
-        print(f"phase_component: {state["phase_component_message"]}")
+        print(f"phase_component: {state["phase_component_formatted"]}")
     if state["workout_schedule_impacted"]:
-        print(f"workout_schedule: {state["workout_schedule_message"]}")
+        print(f"workout_schedule: {state["workout_schedule_formatted"]}")
     print("")
 
     return state
-
-def goal_router(state: AgentState):
-    return state["check"]
-
-def construct_goal_edge(workflow, previous_node, node_if_true, node_if_false):
-    current_checking_node = f"check_{node_if_true}"
-
-    # Advance a successful node to the next check
-    workflow.add_edge(previous_node, current_checking_node)
-    workflow.add_conditional_edges(
-        current_checking_node,
-        goal_router,
-        {
-            True: node_if_true,
-            False: node_if_false
-        }
-    )
-    return None
 
 def create_main_agent_graph():
     workflow = StateGraph(AgentState)
 
     workflow.add_node("user_input_extraction", user_input_information_extraction)
 
-    workflow.add_node("check_availability", check_availability_node)
     workflow.add_node("availability", availability_node)
-
-    workflow.add_node("check_macrocycle", check_macrocycle_node)
     workflow.add_node("macrocycle", macrocycle_node)
-
-    workflow.add_node("check_mesocycle", check_mesocycle_node)
     workflow.add_node("mesocycle", mesocycle_node)
-
-    workflow.add_node("check_microcycle", check_microcycle_node)
     workflow.add_node("microcycle", microcycle_node)
-
-    workflow.add_node("check_phase_component", check_phase_component_node)
     workflow.add_node("phase_component", phase_component_node)
-
-    workflow.add_node("check_workout_schedule", check_workout_schedule_node)
     workflow.add_node("workout_schedule", workout_schedule_node)
-
     workflow.add_node("print_schedule", print_schedule_node)
 
-    # User Input to Availability
-    construct_goal_edge(workflow, "user_input_extraction", "availability", "check_macrocycle")
+    # User Input to Availability and Macrocycles
+    workflow.add_edge("user_input_extraction", "availability")
+    workflow.add_edge("user_input_extraction", "macrocycle")
 
-    # Availability to Macrocycles
-    construct_goal_edge(workflow, "availability", "macrocycle", "check_mesocycle")
-
-    # Macrocycles to Mesocycles
-    construct_goal_edge(workflow, "macrocycle", "mesocycle", "check_microcycle")
+    # Availability and Macrocycles to Mesocycles
+    workflow.add_edge("macrocycle", "mesocycle")
+    workflow.add_edge("availability", "mesocycle")
 
     # Mesocycles to Microcycles
-    construct_goal_edge(workflow, "mesocycle", "microcycle", "check_phase_component")
+    workflow.add_edge("mesocycle", "microcycle")
 
     # Microcycles to Phase Components
-    construct_goal_edge(workflow, "microcycle", "phase_component", "check_workout_schedule")
+    workflow.add_edge("microcycle", "phase_component")
 
     # Phase Components to Workout Exercises
-    construct_goal_edge(workflow, "phase_component", "workout_schedule", "print_schedule")
+    workflow.add_edge("phase_component", "workout_schedule")
 
     # Workout Exercises to End
     workflow.add_edge("workout_schedule", "print_schedule")
