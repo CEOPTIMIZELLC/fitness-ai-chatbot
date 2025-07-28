241c306c1c70c2bf7cf4b38eb477690fb7bb5abb
RanceCL <roderickrwhite2022@gmail.com>
2025-07-28 12:09:06 -0400

    https://app.usemotion.com/web/pm/workspaces/mItBy-ORI682KWmH3G1Bu?task=tk_HEBn6HceqMuhFdcFiPDjhW
    Implemented logger into error handlers.

--- app/error_handling/error_handler.py
+++ app/error_handling/error_handler.py
@@ -1,18 +1,22 @@
 from flask import jsonify
+from logging_config import logger
 
 def not_found_error(e):
+    logger.error(str(e))
     return jsonify({
         "status": "error",
         "message": str(e)
     }), 404
 
 def empty_form_error(e):
+    logger.error(str(e))
     return jsonify({
         "status": "error",
         "message": str(e)
     }), 400
 
 def unauthorized_error(e):
+    logger.error(str(e))
     return jsonify({
         "status": "error",
         "message": str(e)
