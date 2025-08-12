ff1f6cf215e6acb65343fb893eba176832a91270
RanceCL <roderickrwhite2022@gmail.com>
2025-08-12 16:41:26 -0400

    https://app.usemotion.com/web/pm/workspaces/mItBy-ORI682KWmH3G1Bu?task=tk_ewTtYxJfPfAEqu9pWfaFRM
    Added methods to retrieve goals and update the state for the subagents.

--- app/main_agent/base_sub_agents/utils.py
+++ app/main_agent/base_sub_agents/utils.py
@@ -3,6 +3,9 @@ from flask import current_app
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 
+from app.impact_goal_models import RoutineImpactGoals
+from app.goal_prompts import goal_extraction_system_prompt
+
 def sub_agent_focused_items(sub_agent_focus):
     return {
         "entry": f"user_{sub_agent_focus}", 
@@ -40,3 +43,90 @@ def new_input_request(user_input, item_system_prompt, item_goal):
     LogMainSubAgent.parsed_goal(f"")
 
     return goal_class
+
+def user_input_information_extraction(user_input):
+    state={}
+
+    human = f"Extract the goals from the following message: {user_input}"
+    # human = f"New_goal: {user_input}"
+    check_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", goal_extraction_system_prompt),
+            ("human", human),
+        ]
+    )
+    llm = ChatOpenAI(model=current_app.config["LANGUAGE_MODEL"], temperature=0)
+    structured_llm = llm.with_structured_output(RoutineImpactGoals)
+    goal_classifier = check_prompt | structured_llm
+    goal_class = goal_classifier.invoke({})
+
+    state["attempts"] = 1
+    state["availability_impacted"] = goal_class.availability.is_requested
+    state["availability_is_altered"] = True
+    state["availability_message"] = goal_class.availability.detail
+    state["macrocycle_impacted"] = goal_class.macrocycle.is_requested
+    state["macrocycle_is_altered"] = True
+    state["macrocycle_message"] = goal_class.macrocycle.detail
+    state["macrocycle_alter_old"] = goal_class.macrocycle.alter_old
+    state["mesocycle_impacted"] = goal_class.mesocycle.is_requested
+    state["mesocycle_is_altered"] = True
+    state["mesocycle_message"] = goal_class.mesocycle.detail
+    state["microcycle_impacted"] = goal_class.microcycle.is_requested
+    state["microcycle_is_altered"] = True
+    state["microcycle_message"] = goal_class.microcycle.detail
+    state["phase_component_impacted"] = goal_class.phase_component.is_requested
+    state["phase_component_is_altered"] = True
+    state["phase_component_message"] = goal_class.phase_component.detail
+    state["workout_schedule_impacted"] = goal_class.workout_schedule.is_requested
+    state["workout_schedule_is_altered"] = True
+    state["workout_schedule_message"] = goal_class.workout_schedule.detail
+    state["workout_completion_impacted"] = goal_class.workout_completion.is_requested
+    state["workout_completion_is_altered"] = True
+    state["workout_completion_message"] = goal_class.workout_completion.detail
+
+    return state
+
+
+# Update the original value to be the new message if one is present.
+def update_val(final_state, old_state, updated_state, key):
+    final_state[key] = updated_state.get(key, old_state.get(key))
+    return final_state
+
+# Update the boolean to be True if either the current or previous request was true.
+def update_bool(final_state, old_state, updated_state, key):
+    final_state[key] = old_state.get(key, False) or updated_state.get(key, False)
+    return final_state
+
+def agent_state_update(old_state, updated_state):
+    LogMainSubAgent.agent_steps(f"\n=========Update other requests=========")
+    state = {}
+    update_bool(state, old_state, updated_state, "availability_impacted")
+    update_bool(state, old_state, updated_state, "availability_is_altered")
+    update_val(state, old_state, updated_state, "availability_message")
+
+    update_bool(state, old_state, updated_state, "macrocycle_impacted")
+    update_bool(state, old_state, updated_state, "macrocycle_is_altered")
+    update_val(state, old_state, updated_state, "macrocycle_message")
+    update_bool(state, old_state, updated_state, "macrocycle_alter_old")
+
+    update_bool(state, old_state, updated_state, "mesocycle_impacted")
+    update_bool(state, old_state, updated_state, "mesocycle_is_altered")
+    update_val(state, old_state, updated_state, "mesocycle_message")
+
+    update_bool(state, old_state, updated_state, "microcycle_impacted")
+    update_bool(state, old_state, updated_state, "microcycle_is_altered")
+    update_val(state, old_state, updated_state, "microcycle_message")
+
+    update_bool(state, old_state, updated_state, "phase_component_impacted")
+    update_bool(state, old_state, updated_state, "phase_component_is_altered")
+    update_val(state, old_state, updated_state, "phase_component_message")
+
+    update_bool(state, old_state, updated_state, "workout_schedule_impacted")
+    update_bool(state, old_state, updated_state, "workout_schedule_is_altered")
+    update_val(state, old_state, updated_state, "workout_schedule_message")
+
+    update_bool(state, old_state, updated_state, "workout_completion_impacted")
+    update_bool(state, old_state, updated_state, "workout_completion_is_altered")
+    update_val(state, old_state, updated_state, "workout_completion_message")
+
+    return state
