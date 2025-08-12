a260c8f852ce3e17c22b5b38529b58d07231a85b
RanceCL <roderickrwhite2022@gmail.com>
2025-08-12 17:49:12 -0400

    https://app.usemotion.com/web/pm/workspaces/mItBy-ORI682KWmH3G1Bu?task=tk_ewTtYxJfPfAEqu9pWfaFRM
    Made logged microcycle schedule into a more standardized table format.

--- app/schedule_printers/microcycles.py
+++ app/schedule_printers/microcycles.py
@@ -2,40 +2,41 @@ from app.utils.longest_string import longest_string_size_for_key
 from .base import BaseSchedulePrinter
 
 class MicrocycleSchedulePrinter(BaseSchedulePrinter):
-    # def _create_header_fields(self, longest_sizes: dict) -> dict:
-    #     """Create all header fields with consistent formatting"""
-    #     return {
-    #         "number": ("Mesocycle", 12),
-    #         "phase": ("Phase", longest_sizes["phase"] + 4),
-    #         "start_date": ("Start", 17),
-    #         "end_date": ("End", 17),
-    #         "duration": ("Duration", 20),
-    #         "goal_duration": ("Goal Duration", 20),
-    #     }
+    def _create_header_fields(self) -> dict:
+        """Create all header fields with consistent formatting"""
+        return {
+            "number": ("Microcycle", 13),
+            "start_date": ("Start", 17),
+            "end_date": ("End", 17),
+            "duration": ("Duration", 20)
+        }
 
-    # def _line_fields(self, phase):
-    #     return {
-    #         "number": str(str(phase["order"])),
-    #         "phase": f"{phase['phase_name']}",
-    #         "start_date": str(phase["start_date"]),
-    #         "end_date": str(phase["end_date"]),
-    #         "duration": f"{str(phase["duration"])}",
-    #         "goal_duration": f"+{str(phase["duration"]) if phase['is_goal_phase'] else 0}"
-    #     }
+    def _line_fields(self, microcycle):
+        return {
+            "number": str(str(microcycle["order"])),
+            "start_date": str(microcycle["start_date"]),
+            "end_date": str(microcycle["end_date"]),
+            "duration": f"{str(microcycle["duration"])}"
+        }
 
-    # def _log_schedule(self, headers, header_line, schedule):
-    #     schedule_string = ""
-    #     schedule_string += header_line
-    #     for phase in schedule:
-    #         _line_fields = self._line_fields(phase)
-    #         schedule_string += self._formatted_entry_line(headers, _line_fields)
-    #     return schedule_string
+    def _log_schedule(self, headers, header_line, schedule):
+        schedule_string = ""
+        schedule_string += header_line
+        for user_microcycle in schedule:
+            _line_fields = self._line_fields(user_microcycle)
+            schedule_string += self._formatted_entry_line(headers, _line_fields)
+        return schedule_string
 
     def run_schedule_printer(self, schedule):
         formatted = ""
-        formatted = "\n"
-        for user_microcycle in schedule:
-            formatted += f"Microcycle {user_microcycle["order"]}: {user_microcycle["start_date"]} - {user_microcycle["end_date"]} ({user_microcycle["duration"]})\n"
+
+        # Create headers
+        formatted += self.schedule_header
+        headers = self._create_header_fields()
+        header_line = self._formatted_header_line(headers)
+
+        formatted += self._log_schedule(headers, header_line, schedule)
+
         return formatted
 
 def Main(schedule):
