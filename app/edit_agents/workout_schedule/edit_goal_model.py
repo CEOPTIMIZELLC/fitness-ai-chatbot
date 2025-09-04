25f625cb947af69b133b51b120d31a938d05694f
RanceCL <roderickrwhite2022@gmail.com>
2025-09-04 11:11:35 -0400

    https://app.usemotion.com/web/pm/workspaces/mItBy-ORI682KWmH3G1Bu?task=tk_wQh4GFYUxWpfTcbdG9eUWZ
    Added a reasoning field for the LLM to explain why it has included a schedule item and why it has applied certain changes.

--- app/edit_agents/workout_schedule/edit_goal_model.py
+++ app/edit_agents/workout_schedule/edit_goal_model.py
@@ -27,12 +27,15 @@ class Exercise(BaseModel):
     weight: float = Field(
         ..., description="The weight for the exercise in question."
     )
+    reasoning: str = Field(
+        ..., description="An explanation for why the exercise has been included in the edits and why it has had the edits applied."
+    )
 
 
 # Model to extract information on whether the user wants to edit something.
 class WorkoutScheduleEditGoal(BaseModel):
     """Goal extraction from user input regarding editing the current workout schedule."""
-    schedule: List[Exercise] = Field(
+    schedule: Optional[List[Exercise]] = Field(
         [], description="Details about what the values of the schedule elements."
     )
     regenerate: bool = Field(
