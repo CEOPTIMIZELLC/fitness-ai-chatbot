762177a67cee5a67df560ba26ca295414130d385
RanceCL <roderickrwhite2022@gmail.com>
2025-09-29 15:56:51 -0400

    https://app.usemotion.com/web/pm/workspaces/mItBy-ORI682KWmH3G1Bu?task=tk_K8cCKhTha9QKnzi1LWgk2y
    Removed unneeded methods.

--- app/main_agent/user_macrocycles/agent.py
+++ app/main_agent/user_macrocycles/agent.py
@@ -1,8 +1,5 @@
 from langgraph.graph import StateGraph, START, END
 
-from app.db_session import session_scope
-from app.models import User_Macrocycles
-
 from app.main_agent.base_sub_agents.without_parents import BaseAgentWithoutParents as BaseAgent
 from app.main_agent.base_sub_agents.base import confirm_impact, determine_if_create, determine_if_alter, determine_if_read
 from app.impact_goal_models.macrocycles import MacrocycleGoal
@@ -24,45 +21,6 @@ class SubAgent(BaseAgent):
     altering_agent = create_altering_agent()
     creation_agent = create_creation_agent()
     reading_agent = create_reading_agent()
-
-    # Creates the new macrocycle of the determined type.
-    def create_new_macrocycle(self, state: AgentState):
-        user_id = state["user_id"]
-        goal_id = state["goal_id"]
-        new_goal = state["macrocycle_detail"]
-        payload = {}
-        with session_scope() as s:
-            user_macrocycle = User_Macrocycles(user_id=user_id, goal_id=goal_id, goal=new_goal)
-            s.add(user_macrocycle)
-
-            # get PKs without committing the transaction
-            s.flush()
-
-            # load defaults/server-side values
-            s.refresh(user_macrocycle)
-            payload = user_macrocycle.to_dict()
-
-        return {"user_macrocycle": payload}
-
-    # Alters the current macrocycle to be the determined type.
-    def alter_old_macrocycle(self, state: AgentState):
-        goal_id = state["goal_id"]
-        new_goal = state["macrocycle_detail"]
-        macrocycle_id = state["macrocycle_id"]
-        payload = {}
-        with session_scope() as s:
-            user_macrocycle = s.get(User_Macrocycles, macrocycle_id)
-            user_macrocycle.goal = new_goal
-            user_macrocycle.goal_id = goal_id
-
-            # get PKs without committing the transaction
-            s.flush()
-
-            # load defaults/server-side values
-            s.refresh(user_macrocycle)
-            payload = user_macrocycle.to_dict()
-
-        return {"user_macrocycle": payload}
 
     def create_main_agent_graph(self, state_class):
         workflow = StateGraph(state_class)
